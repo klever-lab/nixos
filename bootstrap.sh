1df8ac94e2cf8f1e66f9da34be6d9b48b6cc0197
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-05-18 13:49:13 -0400

    bugs be gone

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,20 @@ then
   exit
 fi
 
+rm -rf /etc/nixos/
+mkdir /etc/nixos/
+cd /etc/nixos/
+nix-shell -p git --run 'git clone https://github.com/klever-lab/nixos ./'
+
+mkdir -p "$HOME/.config/sops/age/"
 echo Decrypt secrets required for bootstrap 
 echo "(Passphrase for decrypting age private key)"
-mkdir -p "$HOME/.config/sops/age/"
-if ! nix-shell -p age --run 'cat sops-nix_primary_key.age age -d > "$HOME/.config/sops/age/keys.txt"';
+if ! nix-shell -p age --run 'cat sops-nix_primary_key.age | age -d > "$HOME/.config/sops/age/keys.txt"';
 then
   echo decrypting age private key failed!!!
   exit
 fi
 
-rm -rf /etc/nixos/
-mkdir /etc/nixos/
-cd /etc/nixos/
-git clone https://github.com/klever-lab/nixos ./
 nixos-generate-config --show-hardware-config > hardware-configuration.nix
 nixos-rebuild switch --upgrade --flake /etc/nixos/
 

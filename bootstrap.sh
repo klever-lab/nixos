37ac4340be221e53f960c5c2e8ade0357a5ff30a
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-05-19 01:37:11 -0400

    YUBIKEYS MOTHERFUCKER

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -eu
 if [[ $(id --user) != 0 ]]
 then
   echo run as root!
-  exit
+  exit 1
 fi
 
 rm -rf /etc/nixos/
@@ -11,17 +11,31 @@ mkdir /etc/nixos/
 cd /etc/nixos/
 nix-shell -p git --run 'git clone https://github.com/klever-lab/nixos ./'
 
+# check if sops-nix decryption keys already present
 if [[ -f "$HOME/.config/sops/age/keys.txt" ]]
 then
-  echo detected "$HOME/.config/sops/age/keys.txt" exists, delete to recreate sops decryption key
+  echo detected "$HOME/.config/sops/age/keys.txt" exists, delete keys.txt to recreate sops decryption key
 else
   mkdir -p "$HOME/.config/sops/age/"
-  echo Decrypt secrets required for bootstrap 
-  echo "(Passphrase for decrypting age private key)"
-  if ! nix-shell -p age --run 'cat sops-nix_primary_key.age | age -d > "$HOME/.config/sops/age/keys.txt"';
+
+  # check if yubikey is plugged in
+  if nix-shell -p lsusb --run 'lsusb | grep Yubikey'
   then
-    echo decrypting age private key failed!!!
-    exit
+    # check if decryption failed
+    # TODO allow for selection of serial and slot
+    if ! nix-shell -p age-plugin-yubikey --run 'age-plugin-yubikey -i --serial 30474330 --slot 1 > "$HOME/.config/sops/age/keys.txt"'
+    then
+      echo accessing yubikey for age private key failed!!!
+      exit 1
+    fi
+  else
+    echo "(Passphrase for decrypting age private key from file)"
+    # check if decryption failed
+    if ! nix-shell -p age --run 'cat sops-nix_primary_key.age | age -d > "$HOME/.config/sops/age/keys.txt"'
+    then
+      echo decrypting age private key failed!!!
+      exit 1
+    fi
   fi
 fi
 

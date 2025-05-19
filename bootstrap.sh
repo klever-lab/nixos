3d866273180975606eee7d5ad201c31029972966
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-05-18 20:18:55 -0400

    qol

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,13 +11,18 @@ mkdir /etc/nixos/
 cd /etc/nixos/
 nix-shell -p git --run 'git clone https://github.com/klever-lab/nixos ./'
 
-mkdir -p "$HOME/.config/sops/age/"
-echo Decrypt secrets required for bootstrap 
-echo "(Passphrase for decrypting age private key)"
-if ! nix-shell -p age --run 'cat sops-nix_primary_key.age | age -d > "$HOME/.config/sops/age/keys.txt"';
+if [[ -f "$HOME/.config/sops/age/keys.txt" ]]
 then
-  echo decrypting age private key failed!!!
-  exit
+  echo detected "$HOME/.config/sops/age/keys.txt" exists, delete to recreate sops decryption key
+else
+  mkdir -p "$HOME/.config/sops/age/"
+  echo Decrypt secrets required for bootstrap 
+  echo "(Passphrase for decrypting age private key)"
+  if ! nix-shell -p age --run 'cat sops-nix_primary_key.age | age -d > "$HOME/.config/sops/age/keys.txt"';
+  then
+    echo decrypting age private key failed!!!
+    exit
+  fi
 fi
 
 nixos-generate-config --show-hardware-config > hardware-configuration.nix
@@ -29,7 +34,7 @@ nix-shell -p git --run 'nixos-rebuild switch --upgrade --flake /etc/nixos/#kleve
 
 
 
-
+# :3
 
 
 

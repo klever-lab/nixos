e01374c06e9f19f45406165d6a501f4097e94e59
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-05-20 11:46:11 -0400

    idk

--- nixosModules/vm_config.nix
+++ nixosModules/vm_config.nix
@@ -11,8 +11,8 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
   boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
+    enable = true;
+    devices = "/dv/vda";
     efiSupport = true;
     efiInstallAsRemovable = true;
   };

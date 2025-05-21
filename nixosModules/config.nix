2af18fb5b92303fc495f1552d5635e2c89455e34
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-05-21 18:36:39 -0400

    idk

--- nixosModules/config.nix
+++ nixosModules/config.nix
@@ -5,15 +5,16 @@
   ...
 }:
 
-boot.loader = {
-  efi = {
-    canTouchEfiVariables = true;
-    efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-  };
-  grub = {
-     efiSupport = true;
-     #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-     device = "nodev";
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
+    grub = {
+       efiSupport = true;
+       #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+       device = "nodev";
+    };
   };
 };
 
